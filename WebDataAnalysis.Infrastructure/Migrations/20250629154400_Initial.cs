1e3827173cad71db2baa226a90dc3ddecce79ec8
pc <pc@HOME-PC>
2025-06-30 00:06:07 +0800

    tttt

---
+++ WebDataAnalysis.Infrastructure/Migrations/20250629154400_Initial.cs
@@ -0,0 +1,198 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDataAnalysis.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Devices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Devices", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Parameteres",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false),
+                    Value = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Parameteres", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Roles",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Roles", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeviceParameters",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeviceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    ParameteresId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceParameters", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceParameters_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id");
+                    table.ForeignKey(
+                        name: "FK_DeviceParameters_Parameteres_ParameteresId",
+                        column: x => x.ParameteresId,
+                        principalTable: "Parameteres",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FIO = table.Column<string>(type: "text", nullable: false),
+                    Login = table.Column<string>(type: "text", nullable: false),
+                    Password = table.Column<string>(type: "text", nullable: false),
+                    RoleId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Users_Roles_RoleId",
+                        column: x => x.RoleId,
+                        principalTable: "Roles",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "FindDevices",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    MAC = table.Column<string>(type: "text", nullable: false),
+                    SpectrumOfWave = table.Column<string>(type: "text", nullable: false),
+                    LenghtOfWave = table.Column<string>(type: "text", nullable: false),
+                    UserId = table.Column<Guid>(type: "uuid", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_FindDevices", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_FindDevices_Users_UserId",
+                        column: x => x.UserId,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.SetNull);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "DeviceMappings",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    FindDeviceId = table.Column<Guid>(type: "uuid", nullable: false),
+                    DeviceId = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_DeviceMappings", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_DeviceMappings_Devices_DeviceId",
+                        column: x => x.DeviceId,
+                        principalTable: "Devices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_DeviceMappings_FindDevices_FindDeviceId",
+                        column: x => x.FindDeviceId,
+                        principalTable: "FindDevices",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceMappings_DeviceId",
+                table: "DeviceMappings",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceMappings_FindDeviceId",
+                table: "DeviceMappings",
+                column: "FindDeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceParameters_DeviceId",
+                table: "DeviceParameters",
+                column: "DeviceId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_DeviceParameters_ParameteresId",
+                table: "DeviceParameters",
+                column: "ParameteresId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_FindDevices_UserId",
+                table: "FindDevices",
+                column: "UserId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Users_RoleId",
+                table: "Users",
+                column: "RoleId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceMappings");
+
+            migrationBuilder.DropTable(
+                name: "DeviceParameters");
+
+            migrationBuilder.DropTable(
+                name: "FindDevices");
+
+            migrationBuilder.DropTable(
+                name: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Parameteres");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+
+            migrationBuilder.DropTable(
+                name: "Roles");
+        }
+    }
+}
---
+++ WebDataAnalysis.Infrastructure/Migrations/20250629160338_AddStatusAndCategory.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebDataAnalysis.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStatusAndCategory : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<Guid>(
+                name: "CategoryId",
+                table: "Devices",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "StatusDeviceId",
+                table: "Devices",
+                type: "uuid",
+                nullable: true);
+
+            migrationBuilder.CreateTable(
+                name: "Category",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Title = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Category", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "StatusDevice",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Status = table.Column<string>(type: "text", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_StatusDevice", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_CategoryId",
+                table: "Devices",
+                column: "CategoryId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Devices_StatusDeviceId",
+                table: "Devices",
+                column: "StatusDeviceId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_Category_CategoryId",
+                table: "Devices",
+                column: "CategoryId",
+                principalTable: "Category",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Devices_StatusDevice_StatusDeviceId",
+                table: "Devices",
+                column: "StatusDeviceId",
+                principalTable: "StatusDevice",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_Category_CategoryId",
+                table: "Devices");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Devices_StatusDevice_StatusDeviceId",
+                table: "Devices");
+
+            migrationBuilder.DropTable(
+                name: "Category");
+
+            migrationBuilder.DropTable(
+                name: "StatusDevice");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_CategoryId",
+                table: "Devices");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Devices_StatusDeviceId",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "CategoryId",
+                table: "Devices");
+
+            migrationBuilder.DropColumn(
+                name: "StatusDeviceId",
+                table: "Devices");
+        }
+    }
+}

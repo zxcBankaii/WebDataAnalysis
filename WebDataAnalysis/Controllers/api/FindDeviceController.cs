c568492f89389e6634e7bc260d9eca5ce100012a
zxcBankaii <168827196+zxcBankaii@users.noreply.github.com>
2025-07-04 10:59:45 +0800

    add controller countFindDeviceByCategory

---
+++ WebDataAnalysis/Controllers/api/FindDeviceController.cs
@@ -0,0 +1,29 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using WebDataAnalysis.Application.UseCase.FindDevices.AllCountFindDevicesByCategory;
+
+namespace WebDataAnalysis.Controllers.api;
+
+[Route("api/find-device")]
+public class FindDeviceController : Controller
+{
+    private readonly IMediator _mediator;
+
+    public FindDeviceController(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+
+    [HttpGet("get-all-count")]
+    public async Task<ActionResult<List<AllCountFindDevicesByCategoryResponse>>> GetAllCountFindDeviceCategory(CancellationToken cancellationToken)
+    {
+        var result = await _mediator.Send(new AllCountFindDevicesByCategoryCommand(), cancellationToken);
+
+        if (!result.IsSuccess)
+        {
+            return StatusCode(result.StatusCode, result.Error);
+        }
+
+        return result.Data;
+    }
+}
